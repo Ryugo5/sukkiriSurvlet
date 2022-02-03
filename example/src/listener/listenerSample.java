b7cef55d2b5c1c8d133922b0efc4212d491bd06c
{古庄龍悟} <{ryugo327@icloud.com}>
2022-02-03 21:08:00 +0900

    20220203_addexample

---
+++ example/src/listener/listenerSample.java
@@ -0,0 +1,39 @@
+package listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class listenerSample
+ *
+ */
+@WebListener
+public class listenerSample implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public listenerSample() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	ServletContext context = sce.getServletContext();
+    	Integer count = 0;
+    	context.setAttribute("count", count);
+    }
+	
+}
---
+++ example/src/servlet/CounterServlet.java
@@ -0,0 +1,63 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CounterServlet
+ */
+@WebServlet("/CounterServlet")
+public class CounterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CounterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//アプリケーションスコープに保存された訪問回数を増加
+		ServletContext application = this.getServletContext();
+		Integer count = (Integer) application.getAttribute("count");
+		count++;
+		application.setAttribute("count", count);
+		
+		//HTMLを出力
+		//response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>訪問回数を表示</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>訪問回数:" + count + "</p>");
+		out.println("<a href=\"CounterServlet\">更新</a>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+	
+	public void destroy() {
+		System.out.println("destroy()が実行されました");
+	}
+
+}
